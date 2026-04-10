49547b491c076c941c15e09356dca8e8d0342e38
diligent.s.r.p <diligent.s.r.p@gmail.com>
2026-04-10 15:57:54 +0800

    fix gpiod package problem

--- IO_control/gpio_test.py
+++ IO_control/gpio_test.py
@@ -1,45 +1,49 @@
-# import gpiod
-
-# BUTTON_LINE_OFFSET = 15
-
-# chip0_button = gpiod.Chip("1", gpiod.Chip.OPEN_BY_NUMBER)
-
-# button = chip0_button.get_line(BUTTON_LINE_OFFSET)
-# button.request(consumer='BUTTON', type=gpiod.LINE_REQ_DIR_IN)
-
-# print(button.consumer())
-
-
-# for i in range(10):
-#     print(button.get_value())
-
-
 import gpiod
+import time
+from gpiod.line import Direction, Value
 
-chip0 = gpiod.Chip("/dev/gpiochip0")
-chip1 = gpiod.Chip("/dev/gpiochip1")
-
-
-# line number = (char - 'A') * 8 + number
-# for example: GPIO1_B2 = chip1 + 1 * 8 + 2 = 10, chip1, line 10
-
-button = chip0.get_line(16)  # GPIO line number
-button.request(consumer="button", type=gpiod.LINE_REQ_DIR_IN)
-
-led_r = chip0.get_line(15)
-led_r.request(consumer="led_r", type=gpiod.LINE_REQ_DIR_OUT)
-led_b = chip1.get_line(7)
-led_b.request(consumer="led_b", type=gpiod.LINE_REQ_DIR_OUT)
-led_g = chip1.get_line(8)
-led_g.request(consumer="led_g", type=gpiod.LINE_REQ_DIR_OUT)
-
-value = button.get_value()
-print("GPIO value:", value)
-
-led_r.set_value(0)
-led_b.set_value(0)
-led_g.set_value(0)
+button_line = 16
+led_r_line = 15
+led_g_line = 8
 
 
+request_chip0 = gpiod.request_lines(
+    "/dev/gpiochip0",
+    consumer="chip0",
+    config={
+        15: gpiod.LineSettings(
+            direction=Direction.OUTPUT, output_value=Value.ACTIVE
+        ),
+        16: gpiod.LineSettings(
+            direction=Direction.INPUT
+        )
+    }
+)
 
+request_chip1 = gpiod.request_lines(
+    "/dev/gpiochip1",
+    consumer="chip1",
+    config={
+        8: gpiod.LineSettings(
+            direction=Direction.OUTPUT, output_value=Value.ACTIVE
+        )
+    }
+)
 
+start_time = 0
+record_state = 0
+
+while True:
+    button_value = request_chip0.get_value(button_line)
+    if button_value == Value.ACTIVE and time.time() - start_time > 3:
+        start_time = time.time()
+        if record_state == 0:
+            record_state = 1
+            request_chip0.set_value(led_r_line, Value.ACTIVE)
+            request_chip1.set_value(led_g_line, Value.INACTIVE)
+        else:
+            record_state = 0
+            request_chip0.set_value(led_r_line, Value.INACTIVE)
+            request_chip1.set_value(led_g_line, Value.ACTIVE)
+        
+        
